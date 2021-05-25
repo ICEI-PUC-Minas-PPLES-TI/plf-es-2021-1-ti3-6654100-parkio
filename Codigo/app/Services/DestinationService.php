132ced087ce1aebeff60704059ede34c31af4d6e
lcs2001 <lcs2001_@hotmail.com>
2021-05-24 21:19:11 -0300

    Create destination crud

--- Codigo/app/Services/DestinationService.php
+++ Codigo/app/Services/DestinationService.php
@@ -8,8 +8,8 @@ use Illuminate\Support\Facades\DB;
 
 class DestinationService
 {
-    
-  /** 
+
+  /**
    * Search for all gates (with pagination)
    *
    * @param String $search Search filter string to find in blocks and/or apartments
@@ -25,13 +25,57 @@ class DestinationService
     return $d->paginate(200);
   }
 
-  public function createDestination($block,int $apartament){
+  public function create($block, $apartament){
     $destination = new Destination();
     $destination->block = strtoupper($block);
     $destination->apartament = $apartament;
+    $destination->deleted_at = null;
     $destination->save();
 
-    return $destination->id;
+    return ['id'=>$destination->id];
+  }
+
+  public function update(int $id, $block, $apartament){
+    $message = 'Destino editado com sucesso';
+
+    $destination = $this->search($id);
+
+    $destination->block = strtoupper($block);
+    $destination->apartament = $apartament;
+    $destination->update();
+    return [
+        'message' => $message,
+        'updated' => true
+    ];
+
+
+}
+
+  public function delete(int $id)
+  {
+      $message = 'Destino removido com sucesso';
+      $this->search($id)->delete();
+      return [
+          'message' => $message,
+          'deleted' => true
+      ];
+
+
+  }
+
+
+  public function search(int $id)
+  {
+      $destination = Destination::find($id);
+
+      if(!empty($destination)){
+
+          return $destination;
+
+      }else {
+          throw new \Exception("Destination Not Found", 404);
+      }
+
   }
 
 }
